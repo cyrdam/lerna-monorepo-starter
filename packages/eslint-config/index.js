00d31b0fd26c6d10e6e5378ce234c0dd305d26f8
cyrdam <51902761+cyrdam@users.noreply.github.com>
2020-07-29 20:44:16 +0200

    feat: create eslint-config package

---
+++ packages/eslint-config/index.js
@@ -0,0 +1,17 @@
+module.exports = {
+  env: {
+    browser: true,
+    es6: true,
+    node: true,
+  },
+  parserOptions: {
+    ecmaVersion: 2019,
+    sourceType: 'module',
+  },
+  rules: {
+    indent: ['error', 2],
+    'linebreak-style': ['error', 'unix'],
+    quotes: ['error', 'single'],
+    semi: ['error', 'always'],
+  },
+};
